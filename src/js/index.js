d97f31b671030551aa51f3d422023ebf1374fa10
jamesjnadeau <james.nadeau@gmail.com>
2019-07-26 19:10:42 -0400

    :tada: add sentry and disable service work env var

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,9 @@
+var Sentry = require('@sentry/browser');
+Sentry.init({ dsn: 'https://bc2d5367cf2b4f0baa7f6344738e2c49@sentry.io/1514838' });
 
 require("bootstrap/dist/js/bootstrap.js");
 
-if ('serviceWorker' in navigator) {
+if ('serviceWorker' in navigator && !env.DISABLE_SERVICEWORKER) {
   // Use the window load event to keep the page load performant
   window.addEventListener('load', function() {
     navigator.serviceWorker.register('/service-worker.js?v=' + env.version);
