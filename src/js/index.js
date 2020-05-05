a337b177b4f5be324a4e0ee195101e315a73b263
jamesjnadeau <james.nadeau@gmail.com>
2020-05-05 17:14:21 -0400

    :fire: disable service worker
    because cache was not working properly

--- src/js/index.js
+++ src/js/index.js
@@ -3,9 +3,16 @@ Sentry.init({ dsn: 'https://bc2d5367cf2b4f0baa7f6344738e2c49@sentry.io/1514838' 
 
 require("bootstrap/dist/js/bootstrap.js");
 
-if ('serviceWorker' in navigator && !env.DISABLE_SERVICEWORKER) {
-  // Use the window load event to keep the page load performant
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/service-worker.js?v=' + env.version);
-  });
+// disabled becuase of caching issue, pages required hard refresh
+// if ('serviceWorker' in navigator && !env.DISABLE_SERVICEWORKER) {
+//   // Use the window load event to keep the page load performant
+//   window.addEventListener('load', function() {
+//     navigator.serviceWorker.register('/service-worker.js?v=' + env.version);
+//   });
+// }
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then(function(registrations) {
+    for(let registration of registrations) {
+     registration.unregister()
+   } });
 }
